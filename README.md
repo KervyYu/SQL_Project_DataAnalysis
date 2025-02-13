40b960ff9afea4cf6ea4b261f97fd8aba8d93ce0
Kervy Yu <kerbs.yu@gmail.com>
2025-02-13 12:52:24 +0800

    added query1 and update question on Readme

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Project: Data Analyst Remote Jobs Analysis
 
 Questions:
 1. What are the top paying Data Analysis Jobs?
-   a.Top paying for Remote? Non-Remote? For both?
+   a.Top paying for Remote? Non-Remote?
 2. What are the top skills required for these top paying remote data analysis job?
 3. Whare skills are the most in-demand for data analysis?
 4. What are the top skills based on salary for data analysis?
---
+++ project_sql/top_paying_DA_jobs.sql
@@ -0,0 +1,11 @@
+/*
+Question:
+1. What are the top paying Data Analysis Jobs?
+   a.Top paying for Remote? Non-Remote? 
+- Find the top 10 mhighest paying data analysis jobs for both remote and non-remote
+- Create (3) queries: 1 for remote jobs only,and 1 for non-remote
+- Remove NULL values for salary
+-Purpose: To be able to highlight the top-paying opportunities for DA, and to know if working remotely as DA has higher pay than non-remote
+*/
+
+
